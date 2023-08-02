91287dee425166cbfe681a22f796b6041e2dd0cb
Blake <blakemiyoung@gmail.com>
2023-08-02 12:26:37 -0400

    name options for name_gen.js complete

--- name_gen.js
+++ name_gen.js
@@ -4,4 +4,7 @@
 const firstName = ['Hamelen', 'Jimothy', 'Krokid', 'Gregory', 'Bhomm', 'Valdriprath', 'Mith', 'Fido'];
 
 //This section will determine the last name of the character
-const lastName = ['Vaz', '']
+const lastName = ['Vaz', 'Drinan', 'Lafer', 'Grimke', 'Balder', 'Algry']
+
+//This section will determine the character's title
+const title = ['the Great', 'the Bearded', 'the Weak', 'the Destroyer', 'the Fearful', 'the Fearless']
