cd37fc839e3077e004ab35d184e8420c8d7c9df4
byoung1007 <136373342+byoung1007@users.noreply.github.com>
2023-08-02 12:13:59 -0400

    Delete name_gen.js

--- name_gen.js
+++
@@ -1,7 +0,0 @@
-//this program will generate names for role playing games.
-
-//This section will determine the first name of the character 
-const firstName = ['Hamelen', 'Jimothy', 'Krokid', 'Gregory', 'Bhomm', 'Valdriprath', 'Mith', 'Fido'];
-
-//This section will determine the last name of the character
-const lastName = ['Vaz', '']
